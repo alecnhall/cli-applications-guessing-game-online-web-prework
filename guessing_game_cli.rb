fff387768e757f16d4320ab700c79a8e419127b8
Alec Hall <alecnhall@gmail.com>
2019-08-09 02:12:53 +0000

    Automatically backed up by Learn

--- guessing_game_cli.rb
+++ guessing_game_cli.rb
@@ -22,7 +22,7 @@ def run_guessing_game
   elsif input == "exit"
     puts "Goodbye!"
   else
-    puts "Sorry! The computer guessed #{random}"
+    puts "Sorry! The computer guessed #{random}."
   end
 end
 
